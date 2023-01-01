c0b6567d834ce12166e5615c809c6f27aad51e4b
yclee6262 <yclee6262@gmail.com>
2023-01-01 21:33:02 +0800

    mainpage

--- frontend/src/container/MainPage.js
+++ frontend/src/container/MainPage.js
@@ -1,5 +1,96 @@
-import Title from "../component/Title";
+import React, { useState } from "react";
+import { FileOutlined, UserOutlined } from "@ant-design/icons";
+import { Breadcrumb, Layout, Menu, theme } from "antd";
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
+}
+const items = [
+  getItem("用戶", "1", <UserOutlined />),
+  getItem("任務", "sub1", <FileOutlined />, [
+    getItem("我發布的任務", "2"),
+    getItem("我接到的任務", "3"),
+  ]),
+];
 const MainPage = () => {
-  return <Title />;
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  return (
+    <Layout
+      style={{
+        minHeight: "100vh",
+      }}
+    >
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+      >
+        <div
+          style={{
+            fontSize: 16,
+            padding: "10px 0",
+            height: 32,
+            margin: 16,
+            color: "#00D600",
+            textAlign: "center",
+          }}
+        >
+          NTU Mission Machine
+        </div>
+        <Menu
+          theme="dark"
+          defaultSelectedKeys={["1"]}
+          mode="inline"
+          items={items}
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        />
+        <Content
+          style={{
+            margin: "0 16px",
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: "16px 0",
+            }}
+          >
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+            }}
+          >
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: "center",
+          }}
+        >
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
 };
 export default MainPage;
