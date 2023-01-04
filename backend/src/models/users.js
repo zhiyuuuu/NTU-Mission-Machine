7409d4e81df2565580bad334fae1d14ca088148b
zhiyuuuu <liuzy0815@gmail.com>
2023-01-05 01:23:18 +0800

    reset db

--- backend/src/models/users.js
+++ backend/src/models/users.js
@@ -4,8 +4,7 @@ const { Schema } = mongoose;
 
 const UserSchema = new Schema({
     name: { type: String, required: [true, 'Name field is required.'] },
-    password: { type: String },
-    tasks: [{ type: mongoose.Types.ObjectId , ref: 'Task'}]
+    password: { type: String }
 }, {
     collection: 'User',
     timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
