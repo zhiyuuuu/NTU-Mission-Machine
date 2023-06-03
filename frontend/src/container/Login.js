3fd77d24fdfee516b9fd374bfb812d2d34c7b83f
zhiyuuuu <b09705038@ntu.edu.tw>
2023-06-03 15:56:27 +0800

    enable authenticate assertion

--- frontend/src/container/Login.js
+++ frontend/src/container/Login.js
@@ -5,6 +5,8 @@ import { useNavigate } from "react-router-dom";
 import { handleLogin, handleCredentialLogin } from "../api";
 import { useState } from "react";
 import "@material/mwc-button";
+import base64url from "base64url";
+import CBOR from "cbor-js";
 
 const Wrapper = styled.div`
   display: flex;
@@ -19,120 +21,194 @@ const Wrapper = styled.div`
 `;
 
 const Login = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const navigateToSignUp = () => {
-    navigate("/reg");
-  };
-  const navigateToMainPage = () => {
-    navigate("/mainpage", {
-      state: {
-        username: username,
-      },
-    });
-  };
+    const navigateToSignUp = () => {
+        navigate("/reg");
+    };
+    const navigateToMainPage = () => {
+        navigate("/mainpage", {
+        state: {
+            username: username,
+        },
+        });
+    };
 
-  const buttonStyle = {
-    backgroundColor: "green",
-  };
+    const buttonStyle = {
+        backgroundColor: "green",
+    };
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-  const [signByPswd, setSignByPswd] = useState(false);
+    const [signByPswd, setSignByPswd] = useState(false);
 
-  const loginTest = async () => {
+    const loginTest = async () => {
     let loginResult = await handleLogin({ username, password });
 
     if (loginResult.message === "success") {
-      navigateToMainPage();
+        navigateToMainPage();
     } else {
-      alert("Username or password is incorrect.");
+        alert("Username or password is incorrect.");
     }
     setUsername("");
     setPassword("");
   };
 
-  const handleSignByPswdOnclick = () => {
-    setSignByPswd(!signByPswd);
-  };
-
-  const dbReturnCredential = async (name) => {
-    const result = await handleCredentialLogin({ name });
-    // console.log("return cred result", result.credential[0]);
-    return result.credential[0];
-  };
-
-  const authenticateCredential = async (name) => {
-    console.log("authenticating");
-    // const { credID, pubKey } = await dbReturnCredential(name); // 等待 Promise 解析
-
-    function stringToArrayBuffer(str) {
-      const encoder = new TextEncoder();
-      return encoder.encode(str).buffer;
-    }
-
-    const result = await dbReturnCredential(name);
-    // console.log(result);
-    const credID = stringToArrayBuffer(result.credID);
-    // console.log(credID);
-
-    const challenge = new Uint8Array(32);
-    window.crypto.getRandomValues(challenge);
-
-    const publicKeyCredentialRequestOptions = {
-      challenge: challenge,
-      allowCredentials: [
-        {
-          id: credID, // from registration
-          type: "public-key",
-          transports: ["usb", "ble", "nfc"],
-        },
-      ],
-      timeout: 60000,
+    const handleSignByPswdOnclick = () => {
+        setSignByPswd(!signByPswd);
     };
 
-    const credential = await navigator.credentials.get({
-      publicKey: publicKeyCredentialRequestOptions,
-    });
-    console.log("credential", credential);
+    const dbReturnCredential = async (name) => {
+        const result = await handleCredentialLogin({ name });
+        // console.log("return cred result", result.credential[0]);
+        return result.credential[0];
+    };
 
-    const assertionResponse = credential.response;
-    const authenticatorData = assertionResponse.authenticatorData; // Get the authenticatorData
-    const clientDataJSON = assertionResponse.clientDataJSON; // Get the clientDataJSON
-    // const hashedDataJSON = ""
-    const signature = assertionResponse.signature; // Get the signature
-    const userHandle = assertionResponse.userHandle; // Get the userHandle
+    const authenticateCredential = async (name) => {
+        console.log("authenticating");
+        const { credID, pubKey } = await dbReturnCredential(name); // 等待 Promise 解析
 
-    // Function to convert Uint8Array to hexadecimal string
-    function arrayBufferToHex(buffer) {
-      return Array.prototype.map
-        .call(new Uint8Array(buffer), (byte) => {
-          return ("00" + byte.toString(16)).slice(-2);
-        })
-        .join("");
-    }
+        function stringToArrayBuffer(str) {
+            const encoder = new TextEncoder();
+            return encoder.encode(str).buffer;
+        }
 
-    // Function to compute SHA-256 hash
-    async function sha256(message) {
-      const encoder = new TextEncoder();
-      const data = encoder.encode(message);
-      const hashBuffer = await crypto.subtle.digest("SHA-256", data);
-      return arrayBufferToHex(hashBuffer);
-    }
+        // const result = await dbReturnCredential(name);
+        // console.log(result);
+        // const credID = stringToArrayBuffer(result.credID);
 
-    const clientDataArray = new Uint8Array(clientDataJSON);
-    const hashedClientData = await sha256(clientDataArray);
+        const credentialIdArray = Object.values(credID);
+        const credentialIdBuffer = new Uint8Array(credentialIdArray).buffer;
+        console.log('credential id buffer', credentialIdBuffer);
 
-    const signedData = authenticatorData + hashedClientData;
-    const isSignatureValid = result.publicKey.verify(signature, signedData);
+        console.log('orig id', credID, 'decoded id', credentialIdBuffer);
 
-    if (isSignatureValid) {
-      navigateToMainPage();
-    } else {
-      alert("login failed");
-    }
-  };
+        const id = localStorage.getItem('credID')
+        const decodedId = Uint8Array.from(id, c => c.charCodeAt(0))
+
+        const challenge = new Uint8Array(32);
+        window.crypto.getRandomValues(challenge);
+        // console.log('decode id', Uint8Array.from(credID, c => c.charCodeAt(0)));
+
+        const publicKeyCredentialRequestOptions = {
+            challenge,
+            allowCredentials: [{
+                id: credentialIdBuffer,
+                type: 'public-key',
+                transports: ['internal'],
+            }],
+            timeout: 60000,
+        }
+
+        const assertion = await navigator.credentials.get({
+            publicKey: publicKeyCredentialRequestOptions,
+        });
+        console.log("assertion", assertion);
+
+        const assertionResponse = assertion.response;
+        const authenticatorData = assertionResponse.authenticatorData; // Get the authenticatorData
+        const clientDataJSON = assertionResponse.clientDataJSON; // Get the clientDataJSON
+        // const hashedDataJSON = ""
+        const signature = assertionResponse.signature; // Get the signature
+        const userHandle = assertionResponse.userHandle; // Get the userHandle
+
+        // // Function to convert Uint8Array to hexadecimal string
+        function arrayBufferToHex(buffer) {
+          return Array.prototype.map
+            .call(new Uint8Array(buffer), (byte) => {
+              return ("00" + byte.toString(16)).slice(-2);
+            })
+            .join("");
+        }
+
+        // // Function to compute SHA-256 hash
+        async function sha256(message) {
+          const encoder = new TextEncoder();
+          const data = encoder.encode(message);
+          const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+          return arrayBufferToHex(hashBuffer);
+        }
+
+        const clientDataArray = new Uint8Array(clientDataJSON);
+        const hashedClientData = await sha256(clientDataArray);
+        console.log('client data JSON', clientDataJSON);
+        console.log('hased client data', hashedClientData);
+
+        const encoder = new TextEncoder();
+        const hashedBuffer = encoder.encode(hashedClientData).buffer;
+
+        // const storedCredential = credentialIdBuffer;
+        console.log('authenticatorData', authenticatorData);
+        const hashedClientDataBuffer = new Uint8Array(hashedClientData).buffer
+        console.log('hashed buffer', hashedBuffer);
+
+        // const signedData = authenticatorData + new Uint8Array(hashedClientData).buffer;
+        function combineArrayBuffers(buffer1, buffer2) {
+            // Create a new buffer with the combined length
+            const combinedLength = buffer1.byteLength + buffer2.byteLength;
+            const combinedBuffer = new ArrayBuffer(combinedLength);
+          
+            // Create views to manipulate the data
+            const view1 = new Uint8Array(buffer1);
+            const view2 = new Uint8Array(buffer2);
+            const combinedView = new Uint8Array(combinedBuffer);
+          
+            // Copy the data from the first buffer to the combined buffer
+            combinedView.set(view1, 0);
+          
+            // Copy the data from the second buffer to the combined buffer
+            combinedView.set(view2, view1.length);
+          
+            return combinedBuffer;
+        }
+
+        const signedData = combineArrayBuffers(authenticatorData, hashedBuffer);
+
+
+        console.log('signed data', signedData);
+        // const isSignatureValid = storedCredential.publicKey.verify(signature, signedData);
+
+        const registrationSignature = signature;
+        const challengeBuffer = challenge;
+
+        const pubKeyArray = Object.values(pubKey);
+        const pubKeyBuffer = new Uint8Array(pubKeyArray).buffer;
+        // console.log('pub key buffer', pubKeyBuffer);
+        const storedPublicKey = pubKeyBuffer;
+        
+        // Convert the registration signature to an ArrayBuffer
+        const signatureBuffer = new Uint8Array(registrationSignature).buffer;
+
+        console.log('storeded public key', storedPublicKey);
+        console.log('signature buffer', signatureBuffer);
+        
+        async function signData(privateKey, data) {
+            const algorithm = { name: 'RSASSA-PKCS1-v1_5', hash: 'SHA-256' };
+            const cryptoKey = await crypto.subtle.importKey(
+              'pkcs8',
+              privateKey,
+              algorithm,
+              false,
+              ['sign']
+            );
+            const signature = await crypto.subtle.sign(algorithm, cryptoKey, data);
+            console.log('signature', signature);
+            return signature;
+        }
+          
+        // Usage example
+        const privateKey = clientDataJSON;
+        const originalData = authenticatorData;
+        const sig = await signData(privateKey, originalData).catch(err => console.log(err));
+
+        console.log(sig);
+        // if (isSignatureValid) {
+        //   navigateToMainPage();
+        // } else {
+        //   alert("login failed");
+        // }
+    };
 
   return (
     <Wrapper>
@@ -152,8 +228,8 @@ const Login = () => {
         <div className="login-option">
           <div id="uvpa_available" class="hidden">
             {/* <h2>
-						Verify your identity
-					</h2> */}
+                        Verify your identity
+                    </h2> */}
             <div>
               <mwc-button
                 id="reauth"
--- frontend/src/container/Register.js
+++ frontend/src/container/Register.js
@@ -10,181 +10,195 @@ import base64url from "base64url";
 import CBOR from "cbor-js";
 
 const Wrapper = styled.div`
-  display: flex;
-  margin-top: 2em;
-  position: relative;
-  width: 100%;
-  overflow-y: auto;
-  justify-content: center;
-  text-align: center;
-  flex-direction: column;
-  align-items: center;
+	display: flex;
+	margin-top: 2em;
+	position: relative;
+	width: 100%;
+	overflow-y: auto;
+	justify-content: center;
+	text-align: center;
+	flex-direction: column;
+	align-items: center;
 `;
 
 const Register = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const navigateToSignIn = () => {
-    navigate("/");
-  };
+	const navigateToSignIn = () => {
+		navigate("/");
+	};
 
-  const [newName, setNewName] = useState("");
-  const [newPswd, setNewPswd] = useState("");
-  const [newCred, setNewCred] = useState([]);
+	const [newName, setNewName] = useState("");
+	const [newPswd, setNewPswd] = useState("");
+	const [newCred, setNewCred] = useState([]);
 
-  const signUp = async () => {
-    let returnMsg = await handleSignUp({ newName, newPswd, newCred });
-    //console.log('return msg: ', returnMsg);
-    if (returnMsg.message === "success") {
-      navigateToSignIn();
-    } else if (returnMsg.message === "existed") {
-      alert("Username already existed Q^Q");
-    } else {
-      alert("Something is wrong :(");
-    }
-  };
+	const signUp = async () => {
+		let returnMsg = await handleSignUp({ newName, newPswd, newCred });
+		console.log('sign up return msg: ', returnMsg);
+		if (returnMsg.message === "success") {
+			navigateToSignIn();
+		} else if (returnMsg.message === "existed") {
+			alert("Username already existed Q^Q");
+		} else {
+			alert("Something is wrong :(");
+		}
+	};
 
-  const registerCredential = async () => {
-    const challenge = new Uint8Array(32);
-    window.crypto.getRandomValues(challenge);
+	const registerCredential = async () => {
+		const challenge = new Uint8Array(32);
+		window.crypto.getRandomValues(challenge);
 
-    const userID = "Kosv9fPtkDoh4Oz7Yq/pVgWHS8HhdlCto5cR0aBoVMw=";
-    const id = Uint8Array.from(window.atob(userID), (c) => c.charCodeAt(0));
+		const userID = "Kosv9fPtkDoh4Oz7Yq/pVgWHS8HhdlCto5cR0aBoVMw=";
+		const id = Uint8Array.from(window.atob(userID), (c) => c.charCodeAt(0));
 
-    const publicKeyCredentialCreationOptions = {
-      challenge,
-      rp: {
-        name: "NTU Mission Machine",
-        id: "localhost",
-      },
-      user: {
-        id,
-        name: newName,
-        displayName: newName,
-      },
-      pubKeyCredParams: [{ alg: -7, type: "public-key" }],
-      authenticatorSelection: {
-        authenticatorAttachment: "platform",
-      },
-      timeout: 60000,
-      // attestation: "direct"
-    };
+		const publicKeyCredentialCreationOptions = {
+			challenge,
+			rp: {
+				name: "NTU Mission Machine",
+				id: "localhost",
+			},
+			user: {
+				id,
+				name: newName,
+				displayName: newName,
+			},
+			pubKeyCredParams: [{ alg: -7, type: "public-key" }],
+			authenticatorSelection: {
+				authenticatorAttachment: "platform",
+			},
+			timeout: 60000,
+			// attestation: "direct"
+		};
 
-    const credential = await navigator.credentials.create({
-      publicKey: publicKeyCredentialCreationOptions,
-    });
+		const credential = await navigator.credentials.create({
+			publicKey: publicKeyCredentialCreationOptions,
+		});
 
-    console.log(credential);
-    const decodeCredential = (credential) => {
-      // decode the clientDataJSON into a utf-8 string
-      const utf8Decoder = new TextDecoder("utf-8");
-      const decodedClientData = utf8Decoder.decode(
-        credential.response.clientDataJSON
-      );
-      // parse the string as an object
-      const clientDataObj = JSON.parse(decodedClientData);
-      console.log("client data object", clientDataObj);
-      // clientDataObj = {
-      // 	challenge: "3woJizj5lDH0h_g2Vnk7gnbow2eynNWidiwD6KdaDSU"
-      // 	crossOrigin: false
-      // 	origin: "http://localhost:3000"
-      // 	type: "webauthn.create"}
+		console.log('id : ', credential, credential.id);
+		const decodeCredential = (credential) => {
+			// decode the clientDataJSON into a utf-8 string
+			const utf8Decoder = new TextDecoder("utf-8");
+			const decodedClientData = utf8Decoder.decode(
+				credential.response.clientDataJSON
+			);
+			// parse the string as an object
+			const clientDataObj = JSON.parse(decodedClientData);
+			console.log("client data object", clientDataObj);
+			// clientDataObj = {
+			// 	challenge: "3woJizj5lDH0h_g2Vnk7gnbow2eynNWidiwD6KdaDSU"
+			// 	crossOrigin: false
+			// 	origin: "http://localhost:3000"
+			// 	type: "webauthn.create"}
 
-      const decodedAttestationObj = CBOR.decode(
-        credential.response.attestationObject
-      );
-      console.log("decoded attestation", decodedAttestationObj);
+			const decodedAttestationObj = CBOR.decode(
+				credential.response.attestationObject
+			);
+			console.log("decoded attestation", decodedAttestationObj);
+			// console.log('user handle', credential.response.userHandle);
 
-      const { authData, attStmt } = decodedAttestationObj;
-      // console.log('auth length', authData.length, attStmt.sig.length);
-      const credentialIdLength = attStmt.sig.length;
-      // get the credential ID
-      const credentialId = authData.slice(55, credentialIdLength);
+			const { authData } = decodedAttestationObj;
 
-      // get the public key object
-      const publicKeyBytes = authData.slice(55 + credentialIdLength);
+			// get the length of the credential ID
+			const dataView = new DataView(
+				new ArrayBuffer(2));
+			const idLenBytes = authData.slice(53, 55);
+			idLenBytes.forEach(
+				(value, index) => dataView.setUint8(
+					index, value));
+			const credentialIdLength = dataView.getUint16();
+			console.log('id length', credentialIdLength);
+			
+			// get the credential ID
+			const credentialId = authData.slice(
+				55, 55 + credentialIdLength);
+			
+			// get the public key object
+			const publicKeyBytes = authData.slice(
+				55 + credentialIdLength);
+			
+			// the publicKeyBytes are encoded again as CBOR
+			const publicKeyObject = CBOR.decode(
+				publicKeyBytes.buffer);
+			console.log('public key object', publicKeyObject)
 
-      // the publicKeyBytes are encoded again as CBOR
-      // console.log('public key bytes', publicKeyBytes);
-      // const publicKeyObject = CBOR.decode(publicKeyBytes.buffer);
-      console.log("data going to save in db", credentialId, publicKeyBytes);
-      setNewCred([
-        {
-          credID: credentialId,
-          pubKey: publicKeyBytes,
-        },
-      ]);
-    };
-    decodeCredential(credential);
-  };
+			console.log("data going to save in db", credentialId, publicKeyBytes);
+			setNewCred([
+				{
+					credID: credentialId,
+					pubKey: publicKeyBytes,
+				},
+			]);
+		};
+		decodeCredential(credential);
+	};
 
-  return (
-    <Wrapper>
-      <Title />
-      <div className="container" style={{ width: "30%" }}>
-        <h1>Sign Up</h1>
-        <p>Please fill in this form to create an account.</p>
-        <hr />
-        <label htmlFor="username">
-          <b>Username</b>
-        </label>
-        <input
-          type="text"
-          placeholder="Enter Username"
-          name="username"
-          required
-          onChange={(e) => setNewName(e.target.value)}
-        />
+	return (
+		<Wrapper>
+			<Title />
+			<div className="container" style={{ width: "30%" }}>
+				<h1>Sign Up</h1>
+				<p>Please fill in this form to create an account.</p>
+				<hr />
+				<label htmlFor="username">
+					<b>Username</b>
+				</label>
+				<input
+					type="text"
+					placeholder="Enter Username"
+					name="username"
+					required
+					onChange={(e) => setNewName(e.target.value)}
+				/>
 
-        <label htmlFor="psw">
-          <b>Password</b>
-        </label>
-        <input
-          type="password"
-          placeholder="Enter Password"
-          name="psw"
-          required
-          onChange={(e) => setNewPswd(e.target.value)}
-        />
+				<label htmlFor="psw">
+					<b>Password</b>
+				</label>
+				<input
+					type="password"
+					placeholder="Enter Password"
+					name="psw"
+					required
+					onChange={(e) => setNewPswd(e.target.value)}
+				/>
 
-        <label htmlFor="psw-repeat">
-          <b>Repeat Password</b>
-        </label>
-        <input
-          type="password"
-          placeholder="Repeat Password"
-          name="psw-repeat"
-          required
-          // onChange={(e) => {
-          //   if (e.target.value !== newPswd) {
-          //     console.log('former :', newPswd, 'latter :', e.target.value);
-          //     alert("Error: Please make sure your password matchs.")
-          //   }
-          // }}
-        />
-        <mwc-button
-          id="register"
-          class="hidden"
-          raised
-          onClick={registerCredential}
-        >
-          Add a credential
-        </mwc-button>
-        <div className="clearfix" style={{ marginTop: "20px" }}>
-          <button
-            type="button"
-            onClick={() => navigateToSignIn()}
-            className="cancelbtn"
-          >
-            Cancel
-          </button>
-          <button type="submit" className="signupbtn" onClick={signUp}>
-            Sign Up
-          </button>
-        </div>
-      </div>
-    </Wrapper>
-  );
+				<label htmlFor="psw-repeat">
+					<b>Repeat Password</b>
+				</label>
+				<input
+					type="password"
+					placeholder="Repeat Password"
+					name="psw-repeat"
+					required
+					// onChange={(e) => {
+					//   if (e.target.value !== newPswd) {
+					//     console.log('former :', newPswd, 'latter :', e.target.value);
+					//     alert("Error: Please make sure your password matchs.")
+					//   }
+					// }}
+				/>
+				<mwc-button
+					id="register"
+					class="hidden"
+					raised
+					onClick={registerCredential}
+				>
+					Add a credential
+				</mwc-button>
+				<div className="clearfix" style={{ marginTop: "20px" }}>
+					<button
+						type="button"
+						onClick={() => navigateToSignIn()}
+						className="cancelbtn"
+					>
+						Cancel
+					</button>
+					<button type="submit" className="signupbtn" onClick={signUp}>
+						Sign Up
+					</button>
+				</div>
+			</div>
+		</Wrapper>
+	);
 };
 
 export default Register;
